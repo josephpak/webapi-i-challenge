0907e8d9642fcb79e57ca611a92013295ae1a3e9
joepak <josephpak2014@gmail.com>
2019-03-18 15:09:37 -0400

    Get server running

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db.js') 
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req,res) => {
+    res.send('Testing')
+})
+
+
+server.listen(9000, () => console.log('API is running on port 9000'))
